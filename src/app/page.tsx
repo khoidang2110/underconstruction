a7f4e44416fb25d5752cee20e64413b7365e23c9
khoidang2110 <khoidang2110@gmail.com>
2024-05-31 20:48:17 +0700

    event end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,13 @@ export default function Home() {
 
 
       <div className="">
-      {/* <a className=" underline underline-offset-1 " href="https://www.glab.vn/product?word=Easybadwork"> */}
-      <a className=" underline underline-offset-1 " href="https://docs.google.com/forms/d/1OGWPOlc3oDcNkCqtursWysbMWsZjI3btxOD8W46444I/viewform?edit_requested=true">
-      {/* <img src="/images/ebwphone.png" className={styles.imagePhone}/>
-      <img src="/images/ebw.png" className={styles.image}/> */}
+      <a className=" underline underline-offset-1 " href="https://www.glab.vn/product?word=Easybadwork">
+      <img src="/images/ebwphone.png" className={styles.imagePhone}/>
+      <img src="/images/ebw.png" className={styles.image}/>
+      
+{/* <a className=" underline underline-offset-1 " href="https://docs.google.com/forms/d/1OGWPOlc3oDcNkCqtursWysbMWsZjI3btxOD8W46444I/viewform?edit_requested=true">
        <img src="/images/newitem.jpg" className={styles.image}/>
-       <img src="/images/newitemmin.jpg" className={styles.imagePhone}/>
+       <img src="/images/newitemmin.jpg" className={styles.imagePhone}/> */}
 
       </a>
    
